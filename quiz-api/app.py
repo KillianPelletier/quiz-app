949c261c1b6bd7198ff69ab9085a12259bad8953
Killian <killian.pelletier@edu.esiee.fr>
2023-05-09 16:23:19 +0200

    Add authentification with JWT

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -1,14 +1,38 @@
-from flask import Flask
+from flask import Flask, request
+from jwt_utils import *
 from flask_cors import CORS
+import hashlib
 
 app = Flask(__name__)
 CORS(app)
 
+# "flask2023"
+PWD_MD5 = b'\xd8\x17\x06PG\x92\x93\xc1.\x02\x01\xe5\xfd\xf4_@'
+
+
 @app.route('/')
 def hello_world():
-	x = 'world'
-	return f"Hello, {x}"
+    x = 'world'
+    return f"Hello, {x}"
 
+
+@app.route('/quiz-info', methods=['GET'])
+def GetQuizInfo():
+    return {"size": 0, "scores": []}, 200
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    payload = request.get_json()
+    pwd = payload['password'].encode('UTF-8')
+    pwd_hashed = hashlib.md5(pwd).digest()
+    if pwd_hashed == PWD_MD5:
+        token = build_token()
+        return {"token": token}, 200
+    else:
+        return 'Unauthorized', 401
+
+
+# Keep at the end
 if __name__ == "__main__":
     app.run()
-
