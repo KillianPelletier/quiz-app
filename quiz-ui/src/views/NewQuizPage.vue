cb2c419e9df006b420823129ddb43bf36c3f8c34
Killian <killian.pelletier@edu.esiee.fr>
2023-05-21 13:21:55 +0200

    Implement NewQuizPage

---
+++ quiz-ui/src/views/NewQuizPage.vue
@@ -0,0 +1,34 @@
+
+<template>
+  <div>
+    <p>Saisissez votre nom : </p>
+    <input type="text" v-model="username">
+    <button @click="launchNewQuiz">Go !</button>
+  </div>
+</template>
+
+<script>
+import participationStorageService from "@/ParticipationStorageService";
+
+export default {
+  name: "NewQuizPage",
+
+  data() {
+    return {
+      username:''
+    }
+  },
+
+  methods:{
+    launchNewQuiz(){
+      participationStorageService.savePlayerName(this.username);
+      console.log("Launch new quiz with", this.username);
+      this.$router.push('/questions');
+    },
+  },
+
+  async created() {
+    console.log("Composant NewQuizPage page 'created'");
+  }
+};
+</script>
