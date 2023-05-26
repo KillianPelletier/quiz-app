055dea2097707dc698f4db3ed8467e961dfabcac
Louis PREVOST <louis77.prevost@hotmail.fr>
2023-05-27 00:40:32 +0200

    Heineken font

--- quiz-ui/src/views/AboutView.vue
+++ quiz-ui/src/views/AboutView.vue
@@ -1,30 +1,45 @@
 <template>
-  <h1 class="white">À propos</h1>
+  <h1 class="white">A propos</h1>
 
   <p>
-  Bienvenue sur notre site dédié au quizz sur l'alcool, où chaque réponse est accompagnée d'un ton humoristique et légèrement... enivrant ! Ici, nous avons fusionné deux grandes passions : les connaissances et l'appréciation des breuvages festifs.
+    Bienvenue sur notre site dédié au quizz sur l'alcool, où chaque réponse est
+    accompagnée d'un ton humoristique et légèrement... enivrant ! Ici, nous
+    avons fusionné deux grandes passions : les connaissances et l'appréciation
+    des breuvages festifs.
   </p>
-  <br>
+  <br />
   <p>
-  Nous sommes convaincus que l'apprentissage n'a jamais été aussi amusant qu'avec un verre à la main (à condition d'être en âge légal, bien sûr). Alors, si vous souhaitez tester vos connaissances tout en trinquant virtuellement avec nous, vous êtes au bon endroit !
+    Nous sommes convaincus que l'apprentissage n'a jamais été aussi amusant
+    qu'avec un verre à la main (à condition d'être en âge légal, bien sûr).
+    Alors, si vous souhaitez tester vos connaissances tout en trinquant
+    virtuellement avec nous, vous êtes au bon endroit !
   </p>
-  <br>
+  <br />
   <p>
-  Notre équipe d'experts en "bourre"-ologie (un terme scientifique très sérieux) a minutieusement concocté des questions et des réponses pour vous faire sourire, même si vous avez la gueule de bois. Nous croyons fermement que la culture et le divertissement peuvent se marier aussi bien que le vin et le fromage !
+    Notre équipe d'experts en "bourre"-ologie (un terme scientifique très
+    sérieux) a minutieusement concocté des questions et des réponses pour vous
+    faire sourire, même si vous avez la gueule de bois. Nous croyons fermement
+    que la culture et le divertissement peuvent se marier aussi bien que le vin
+    et le fromage !
   </p>
-  <br>
+  <br />
   <p>
-  Que vous soyez un amateur curieux, un connaisseur éclairé ou un véritable sommelier de la dive bouteille, notre quizz vous mettra à l'épreuve avec des énigmes, des anecdotes et des jeux de mots dignes d'un humoriste ivre (ou presque).
+    Que vous soyez un amateur curieux, un connaisseur éclairé ou un véritable
+    sommelier de la dive bouteille, notre quizz vous mettra à l'épreuve avec des
+    énigmes, des anecdotes et des jeux de mots dignes d'un humoriste ivre (ou
+    presque).
   </p>
-  <br>
+  <br />
   <p>
-  Alors, asseyez-vous confortablement, prenez un verre (avec modération, bien sûr) et préparez-vous à boire... des connaissances ! Notre site est là pour vous désaltérer intellectuellement tout en vous faisant sourire. Santé !
+    Alors, asseyez-vous confortablement, prenez un verre (avec modération, bien
+    sûr) et préparez-vous à boire... des connaissances ! Notre site est là pour
+    vous désaltérer intellectuellement tout en vous faisant sourire. Santé !
   </p>
 </template>
 
 <style>
-p{
+p {
   font-size: 15pt;
-  color:white;
+  color: white;
 }
 </style>
